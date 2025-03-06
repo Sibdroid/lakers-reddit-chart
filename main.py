189b8588a5d542b3974550d68adaa426ed0a8908
Sibdroid <ryabovglebrg@gmail.com>
2025-03-06 13:38:20 +0100

    feat: make basic charts

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pandas as pd
 from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def moving_average(a, n=10):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
 
 
 class SeasonData:
@@ -10,7 +18,7 @@ class SeasonData:
         self.START_DATE = self.df["DATE"].iloc[0]
         self.x = self.df["DATE"].apply(lambda x: self._date_to_int(x, self.START_DATE))
         self.y = self.df["W-L"].apply(lambda x: self._ratio_to_percentage(x))
-        self.x_labels = self.df["DATE"].apply(
+        self.x_labels = self.df["DATE"].apply(lambda x: self._shorten_date(x))
 
 
     def _ratio_to_percentage(self, ratio: str) -> float:
@@ -34,9 +42,21 @@ class SeasonData:
 
 
     def _shorten_date(self, date: str) -> str:
-        ...
+        return date.split(", ")[1]
 
 
-data = SeasonData("data.xlsx")
-print(data.x)
-print(data.y)
+    def __repr__(self):
+        return "A class containing season data"
+
+
+def main():
+    data = SeasonData("data.xlsx")
+    plt.style.use('fivethirtyeight')
+    fig, ax = plt.subplots()
+    ax.set_ylim([0, 102])
+    ax.plot(data.x, data.y, color = "#6938BC", linewidth = 4)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
